672fa911584b16521a063f8e3a6c272efcc8fd30
Jarvis <13g7895123@gmail.com>
2022-09-13 16:16:07 +0800

    按鈕動作(未完成)

--- Pages/js/tools_material_price.js
+++ Pages/js/tools_material_price.js
@@ -1,18 +1,41 @@
+// 設定初始值
+$('input[name="material_name"]').val('');
+$('input[name="weight"]').val('');
+$('input[name="total_price"]').val('');
+
 // 取得輸入數值
 var material = $('input[name="material_name"]').val();
 var weight = $('input[name="weight"]').val();
 var unit = $('select[name="unit"]').val();
 var total_price = $('input[name="total_price"]').val();
 
-// 確認不為空值
+var list_count = 0;
 
 $('.add').click(function () {
 
-    // 新增BOT項目
-    $('.bot').append(
+    // 確定不為空值
+    if (material == '') {
+        alert('數值有誤，請重新確認!');
+    } else if (material != '' && weight != '' && total_price != '') {
+        alert('click');
+        list_count += 1;
+        // 新增BOT項目
+        $('.bot').append(
+            "<div class='list'>" +
+            "<div class='list_field se_num'>" + list_count + ".</div>" +
+            "<div class='list_field name'>" + material + "</div>" +
+            "<div class='list_field weight'>" + weight + "</div>" +
+            "<div class='list_field unit'>" + unit + "</div>" +
+            "<div class='list_field total_price'>" + total_price + "元</div>" +
+            "<div class='list_field arrow'><i class='fa-solid fa-arrow-right'></i></div>" +
+            "<div class='list_field g_price'>$" + "(g)</div>" +
+            "<div class='list_field btn_cancel'>X</div>" +
+            "</div>"
+        );
 
-    );
-
-    // 新增最後確定項目
-
+        // 新增最後確定項目
+    }
 });
+
+
+
--- Pages/tools_material_price.php
+++ Pages/tools_material_price.php
@@ -7,19 +7,30 @@
         </div>
         <div class='mid'>
             <label class='field material'>材料:</label>
-            <input class='material_name' name='material_name'>
+            <input class='material_name' name='material_name' required>
             <label class='field weight'>重量:</label>
-            <input name='weight'>
+            <input name='weight' required>
             <select name='unit'>
                 <option>公斤</option>
                 <option>公克</option>
                 <option>台斤</option>
             </select>
             <label class='field price'>價格:</label>
-            <input name='total_price'>
+            <input name='total_price' required>
             <div class='btn add'>+新增</div>
         </div>
-        <div class='bot'></div>
+        <div class='bot'>
+            <!-- <div class='list'>
+                <div class='list_field se_num'>1.</div>
+                <div class='list_field name'>高筋麵粉</div>
+                <div class='list_field weight'>100</div>
+                <div class='list_field unit'>公斤</div>
+                <div class='list_field total_price'>300元</div>
+                <div class='list_field arrow'><i class="fa-solid fa-arrow-right"></i></div>
+                <div class='list_field g_price'>$0.3(g)</div>
+                <div class='list_field btn_cancel'>X</div>
+            </div> -->
+        </div>
 
 
     </div>
